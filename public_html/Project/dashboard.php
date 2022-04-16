380227148a39b51572f90e40650764de72bc726f
Marwan Baroud <mb25@njit.edu>
2022-04-16 17:54:20 -0400

    adding MS2 dashboardpage and transactiontable

---
+++ public_html/Project/dashboard.php
@@ -0,0 +1,28 @@
+<?php
+require (__DIR__ . "/../../partials/nav.php"); 
+?>
+<h1>Dashboard</h1>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-warning">
+<div class="container-fluid">
+
+<div class="collapse navbar-collapse" id="navContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+               <li class="nav1-item"> <a class="nav-link" href="<?php echo get_url('create_acct.php'); ?>"> Create Account </a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo get_url('profile.php'); ?>"> Profile </a></li>
+                <li class ="nav-item " > <a class="nav-link" href="<?php echo get_url('transaction.php?type=deposit'); ?>"> Deposit </a></li>
+                <li class ="nav-item" > <a class="nav-link" href="<?php echo get_url('transaction.php?type=withdraw'); ?>"> Withdraw </a></li>
+                <li class ="nav-item" > <a class="nav-link" href="# "> Transfer </a></li>
+                <li class ="nav-item" > <a class="nav-link" href="<?php echo get_url('view_accounts.php'); ?>"> My Accounts </a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<?php
+require(__DIR__ . "/../../partials/flash.php");
+?>
---
+++ public_html/Project/transaction.php
@@ -0,0 +1,109 @@
+<?php
+require_once(__DIR__ . '/../../partials/nav.php');
+
+use function PHPSTORM_META\type;
+
+if (!is_logged_in()){
+    flash("You need to login first!");
+    die(header("Location: login.php"));
+}
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$srcID = get_user_id();
+$db = getDB();      
+$stmt = $db->prepare("SELECT id, account_number from Accounts WHERE user_id=:user_id order by created DESC LIMIT 10");
+$r = $stmt->execute([":user_id"=>get_user_id()]);
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$q = "SELECT id from Accounts WHERE account_number='000000000000'";
+$stmt = $db->prepare($q);
+	$s = $stmt->execute();
+	$res = $stmt->fetch(PDO::FETCH_ASSOC);
+$worldID = $res["id"];
+?>
+
+<form method="POST"> 
+	<label for "source"><h3> Source Account</h3></label>
+	<select name="source" id="source">
+	  <?php foreach($results as $r):?>
+	    <option value="<?php se($r["id"]);?>"><?php se($r["account_number"]);?></option>
+	  <?php endforeach;?>
+	</select>
+	<?php if($_GET == 'transfer') : ?>
+
+	<label for "dest"><h3> Destination Account</h3></label>
+	<select name="dest" id="dest">
+          <?php foreach($results as $r):?>
+            <option value="<?php se($r["id"]);?>"><?php se($r["account_number"]);?></option>
+          <?php endforeach;?>
+        </select>
+	<?php endif; ?>
+	<!-- <?php if($_GET == 'ext_transfer') : ?> -->
+        <!-- <label for "dest"><h3> Destination Account</h3></label> -->
+        <!-- <select name="dest" id="dest"> -->
+          <!-- <?php foreach($results as $r):?> -->
+            <!-- <option value="<?php se($r["id"]);?>"><?php se($r["account_number"]);?></option> -->
+          <!-- <?php endforeach;?> -->
+        <!-- </select> -->
+        <!-- <?php endif; ?> -->
+	
+	<input type="number" name="amount" placeholder="amount"/>
+	<input type="text" name="memo" placeholder="Memo" />
+	<input type="submit" value="Transfer" name="submit"/>
+</form>
+
+
+<?php
+
+if(isset($_POST['submit'])){
+	if(isset($_POST['source']) && isset($_POST['amount'])){
+		
+		$type = $_GET['type'];
+		$BalanceChange = (int)$_POST['amount'];
+		$memo = "";
+		$isvalid = true;
+		if($BalanceChange <= 0){
+			flash("Amount must be greater than 0!");
+			$isvalid = false;
+		}
+		if(isset($_POST['memo']) && !empty($_POST['memo'])){
+		   $memo = $_POST['memo'];
+		}
+		else{
+		   $memo = "N/A";
+		}
+	}
+	if(isset($_POST['dest']) && $_POST['source'] == $_POST['dest']){
+	    flash("Source and Destination account can not be the same!");
+	    $isvalid = false;
+	}
+		if($isvalid){
+
+	    switch($type){
+
+		case 'deposit':
+			do_bank_action($worldID, $_POST['source'], ($BalanceChange), '0', $memo);
+			flash("Deposit Successful!");
+			break;
+		case 'withdraw':
+			if(getRealTimeBalance($_POST['source']) >= $BalanceChange){
+			    do_bank_action($_POST['source'], $worldID, ($BalanceChange), '1', $memo);
+			    flash("Withdrawal Successful!");
+			}else{
+			    flash("You do not have enough to withdraw this amount");
+			}break;
+		case 'transfer':
+			if(getRealTimeBalance($_POST['source']) >= $BalanceChange){
+			    do_bank_action($_POST['source'], $_POST['dest'], ($BalanceChange * -1), '2', $memo);
+			    flash("Your transaction has successfully been posted!");
+			}else{
+				flash("You do not have enough to transfer this amount");
+			}break;
+	   }
+		}}
+
+require(__DIR__ . '/../../partials/flash.php');
+?>
